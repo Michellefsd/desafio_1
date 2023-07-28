8913b2c3dff9ebd3b48f34a280fe5dc12aef0e3f
Michellefsd <promichfsd@gmail.com>
2023-07-28 16:49:08 -0300

    bien ahora esta funcional. no hay animacion en la hamburguesa y parece q no puedo tocar los links

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,18 +1,30 @@
 "use strict";
 
-// Selecciono el boton  y el nev mediante vanilla javascript
+// variables//
 var btn = document.querySelector('.menu-button');
-var nav = document.querySelector('#nav');
-nav.addEventListener("mouseout", hideNav);
-btn.addEventListener("mouseover", displayNav);
+var title = document.querySelector('#title');
+header = document.querySelector('header');
+var nav = document.querySelector('nav'); // eventlisteners
+
+nav.classList.add('none');
+btn.addEventListener("click", displayNav);
+header.addEventListener("mouseleave", hideNav); // functions
+// con el toggle hago que se invierta la visibilidad al darle click al menu
 
 function displayNav() {
-  nav.style.display.remove = "none";
-  nav.style.display = "block";
-}
+  title.classList.toggle('none');
+  nav.classList.toggle('nav');
+  nav.classList.toggle('none');
+  console.log('display');
+} // Y en esta especifico que hacer si uno se aleja del nav
+
 
 function hideNav() {
-  nav.style.display.remove = "block";
-  nav.style.display = "none";
+  if (title.classList.contains('none')) {
+    title.classList.remove('none');
+    nav.classList.remove('nav');
+    nav.classList.add('none');
+    console.log('hide');
+  }
 }
 //# sourceMappingURL=script.dev.js.map
--- script.js
+++ script.js
@@ -1,17 +1,36 @@
+ // variables//
 
-// Selecciono el boton  y el nev mediante vanilla javascript
-const btn = document.querySelector('.menu-button');
-const nav = document.querySelector('#nav');
+    const btn = document.querySelector('.menu-button');
+    const title = document.querySelector('#title');
+    header = document.querySelector('header');
+    const nav = document.querySelector('nav');
 
-nav.addEventListener("mouseout", hideNav);
-btn.addEventListener("mouseover", displayNav);
 
-function displayNav() {
-    nav.style.display.remove = "none";
-    nav.style.display = "block";
-}
+    // eventlisteners
+    nav.classList.add('none');
+    btn.addEventListener("click", displayNav);
+    header.addEventListener("mouseleave", hideNav);
 
-function hideNav() {
-    nav.style.display.remove = "block";
-    nav.style.display = "none";
-}
+    // functions
+
+    // con el toggle hago que se invierta la visibilidad al darle click al menu
+    function displayNav() {
+        title.classList.toggle('none')
+        nav.classList.toggle('nav');
+        nav.classList.toggle('none');
+        console.log('display');
+
+    }
+
+
+    // Y en esta especifico que hacer si uno se aleja del nav
+    function hideNav() {
+        if(title.classList.contains('none')) {
+            title.classList.remove('none')
+            nav.classList.remove('nav');
+            nav.classList.add('none');
+            console.log('hide');
+        }
+
+
+    }
