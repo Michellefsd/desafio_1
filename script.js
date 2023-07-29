1ade5cd3971d6df85df834f6c5518527a02dcb25
Michellefsd <promichfsd@gmail.com>
2023-07-29 09:17:04 -0300

    improving menu burguer functionality

--- script.js
+++ script.js
@@ -4,33 +4,45 @@
     const title = document.querySelector('#title');
     header = document.querySelector('header');
     const nav = document.querySelector('nav');
+    const menuIcon = document.querySelector('.menu-icon');
 
 
     // eventlisteners
     nav.classList.add('none');
-    btn.addEventListener("click", displayNav);
+    btn.addEventListener("click", doEverything);
     header.addEventListener("mouseleave", hideNav);
 
     // functions
 
-    // con el toggle hago que se invierta la visibilidad al darle click al menu
-    function displayNav() {
-        title.classList.toggle('none')
-        nav.classList.toggle('nav');
-        nav.classList.toggle('none');
-        console.log('display');
-
+    function doEverything() {
+        displayNav();
+        animateMenu();
+        removeText();
     }
 
+    // con el toggle hago que se invierta la visibilidad al darle click al menu
+    function displayNav() {
+        nav.classList.toggle('nav');
+        nav.classList.toggle('none');
+    }
+    
+    function animateMenu() {
+        menuIcon.classList.toggle("moving");
+        menuIcon.classList.add("before-before");
+        menuIcon.classList.add("after-after");
+    }
+    function removeText() {
+        title.classList.toggle('none')
+    }
 
     // Y en esta especifico que hacer si uno se aleja del nav
     function hideNav() {
         if(title.classList.contains('none')) {
+            menuIcon.classList.remove('moving');
             title.classList.remove('none')
             nav.classList.remove('nav');
             nav.classList.add('none');
             console.log('hide');
         }
-
-
     }
+    
